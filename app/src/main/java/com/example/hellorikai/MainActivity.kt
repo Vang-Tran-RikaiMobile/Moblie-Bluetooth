02281a516f9a8cddb7ea92b2d0ed395185d001a6
Vang-Tran-Rikai <vang.tran@rikai.technology>
2024-10-01 17:37:30 +0700

    Setup Bluetooth

--- app/src/main/java/com/example/hellorikai/MainActivity.kt
+++ app/src/main/java/com/example/hellorikai/MainActivity.kt
@@ -1,12 +1,21 @@
 package com.example.hellorikai
 
+import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothManager
 import android.os.Bundle
+import android.widget.Switch
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.SwitchCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    val bluetoothManager: BluetoothManager = getSystemService(BluetoothManager::class.java)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -15,6 +24,15 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val swBluetooth: Switch = findViewById<Switch>(R.id.swBT)
+        val bluetoothApt: BluetoothAdapter? = bluetoothManager.adapter
+        if (bluetoothApt == null){
+            Toast.makeText( this, "This device does not support Bluetooth. Operations cannot be performed"
+            , Toast.LENGTH_LONG).show()
+        }else{
+            Toast.makeText( this, "Bluetooth support is successfully Detected!!"
+                , Toast.LENGTH_LONG).show()
         }
     }
 }
